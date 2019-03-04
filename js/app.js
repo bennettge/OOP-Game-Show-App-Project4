e0b2bfa9014ae90c47402505bbfa75c7a5b81aab
George Austin Bennett <bennettge@thayer.org>
2019-03-04 11:16:52 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  * app.js */
 
 // Instantiation of currGame object
-const currGame = new Game(listOfPhrases);
+const currGame = new Game();
 
 // Event listener for start button
 const startButton = document.getElementById("btn__reset");
